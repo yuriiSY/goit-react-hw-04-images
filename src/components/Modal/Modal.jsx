600be59813e782560a9d14e62208eaee0a4daf9d
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2024-01-24 20:41:33 +0000

    cleaning

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -29,36 +29,4 @@ const Modal = ({ close, modalData }) => {
   );
 };
 
-// class Modal extends Component {
-//   componentDidMount() {
-//     document.addEventListener('keydown', this.closeModal);
-//   }
-
-//   componentWillUnmount() {
-//     document.removeEventListener('keydown', this.closeModal);
-//   }
-
-//   closeModal = ({ target, currentTarget, code }) => {
-//     if (target === currentTarget || code === 'Escape') {
-//       this.props.close();
-//     }
-//   };
-
-//   render() {
-//     const { modalData, close } = this.props;
-
-//     return createPortal(
-//       <div onClick={this.closeModal} className="Overlay">
-//         <div className="Modal">
-//           <span onClick={close} className="Close">
-//             X
-//           </span>
-//           <img src={modalData.largeImageURL} alt="" />
-//         </div>
-//       </div>,
-//       modalRoot
-//     );
-//   }
-// }
-
 export default Modal;
